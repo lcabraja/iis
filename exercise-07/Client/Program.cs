29360f6cd86a31d6bd717a8c0e48edb872200972
Luka Čabraja <luka@cabraja.eu>
2022-07-01 18:55:24 +0200

    Add files via upload

---
+++ exercise-07/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorService.CalculatorSoapClient client = new CalculatorService.CalculatorSoapClient(CalculatorService.CalculatorSoapClient.EndpointConfiguration.CalculatorSoap);
+            
+            Console.WriteLine(client.SumAsync(1, 2).Result);
+            Console.WriteLine(client.SubtractAsync(3,1).Result);
+
+            double value = 100;
+            double results = client.PriceAsync(value,4 ).Result;
+            Console.WriteLine(results);
+
+
+        }
+    }
+}
---
+++ exercise-07/Server/Calculator.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Server
+{
+    /// <summary>
+    /// Summary description for Calculator
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Calculator : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        [WebMethod]
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        [WebMethod]
+        public double Price(double sansTax, int transactionType)
+        {
+            switch (transactionType)
+            {
+                case 1:
+                    return sansTax * 1.05;
+                case 2:
+                    return sansTax * 1.13;
+                case 3:
+                    return sansTax * 1.25;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
