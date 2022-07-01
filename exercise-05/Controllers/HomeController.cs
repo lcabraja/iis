a5bfae53854d39fb6067e6566b0845432d6b51ec
Luka Čabraja <luka@cabraja.eu>
2022-07-01 18:55:06 +0200

    Add files via upload

---
+++ exercise-05/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using exercise_05.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exercise_05.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int num1, int num2)
+        {
+            ViewBag.result = num1 + num2;
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
